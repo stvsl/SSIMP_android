ee59d82b7cece80deb506d62fffb932c4815dc41
stvsl <aptx4869stvsl@outlook.com>
2023-03-25 15:40:33 +0800

    补丁

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import router from "./router";
 import { IonicVue } from "@ionic/vue";
 // Above the createApp() line
 import { defineCustomElements } from "@ionic/pwa-elements/loader";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 // Call the element loader after the platform has been bootstrapped
 defineCustomElements(window);
@@ -27,8 +30,12 @@ import "@ionic/vue/css/display.css";
 
 /* Theme variables */
 import "./theme/variables.css";
+library.add(faChevronLeft);
 
-const app = createApp(App).use(IonicVue).use(router);
+const app = createApp(App)
+  .use(IonicVue)
+  .use(router)
+  .component("font-awesome-icon", FontAwesomeIcon);
 
 router.isReady().then(() => {
   app.mount("#app");
--- src/views/WorkPage.vue
+++ src/views/WorkPage.vue
@@ -1,9 +1,46 @@
 <template>
-  <div>111</div>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>
+          <ion-button fill="clear" size="large" href="/home/task">
+            <font-awesome-icon :icon="['fas', 'chevron-left']" />
+            任务</ion-button>
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">任务</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script setup lang="ts">
 
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+ion-button {
+  --background: transparent;
+  --background-activated: transparent;
+  --background-hover: transparent;
+  --background-focused: transparent;
+  --color: #000;
+  --padding-start: 0;
+  /* 点击动画取消 */
+  --ripple-color: transparent;
+  --ripple-opacity: 0;
+}
+
+svg {
+  width: 1rem;
+  height: 1rem;
+  margin-right: 0.5rem;
+}
+</style>
